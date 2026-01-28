999f724ff8f6c7607b31052a96f4f18dfe78e19f
Dave Snowdon <dave@davesnowdon.com>
2026-01-28 07:49:51 +0000

    start anim blend code

--- ui/player.gd
+++ ui/player.gd
@@ -1,6 +1,15 @@
 extends CharacterBody2D
 
 @export var move_speed: float = 100
+@export var starting_direction: Vector2 = Vector2(0, 0.5)
+
+# parameters/idle/blend_position
+# parameters/walk/blend_position
+
+@onready var animation_tree = $AnimationTree
+
+func _ready() -> void:
+	animation_tree.set("parameters/idle/blend_position", starting_direction)
 
 func _physics_process(_delta: float) -> void:
 	var input_direction = Vector2(
