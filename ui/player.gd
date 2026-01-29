a73423ad2bc54a02ac8fed7f874319acadcc6ca1
Dave Snowdon <dave@davesnowdon.com>
2026-01-29 09:15:45 +0000

    player state machine

--- ui/player.gd
+++ ui/player.gd
@@ -3,20 +3,32 @@ extends CharacterBody2D
 @export var move_speed: float = 100
 @export var starting_direction: Vector2 = Vector2(0, 0.5)
 
-# parameters/idle/blend_position
-# parameters/walk/blend_position
-
 @onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree.get("parameters/playback")
 
 func _ready() -> void:
-	animation_tree.set("parameters/idle/blend_position", starting_direction)
+	update_animaton_parameters(starting_direction)
 
 func _physics_process(_delta: float) -> void:
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
+	
+	update_animaton_parameters(input_direction)
 
 	velocity = input_direction * move_speed
+	pick_new_state()
 
 	move_and_slide()
+
+func update_animaton_parameters(move_input : Vector2):
+	if (move_input != Vector2.ZERO):
+		animation_tree.set("parameters/idle/blend_position", move_input)
+		animation_tree.set("parameters/walk/blend_position", move_input)
+
+func pick_new_state():
+	if (velocity != Vector2.ZERO):
+		state_machine.travel("walk")
+	else:
+		state_machine.travel("idle")
